bf49b6a8b81d89aa9cc7697ed0042c2af3873e02
Joel Sing <joel@sing.id.au>
2019-05-05 19:03:42 +1000

    Remove FIXME that is fixed.

--- src/syscall/asm_linux_riscv.s
+++ src/syscall/asm_linux_riscv.s
@@ -8,7 +8,6 @@
 // System calls for riscv64, Linux
 //
 
-// FIXME call entersyscall/exitsyscall once the runtime is working
 // func Syscall(trap int64, a1, a2, a3 int64) (r1, r2, err int64)
 TEXT ·Syscall(SB),NOSPLIT,$0-56
 	CALL	runtime·entersyscall(SB)
