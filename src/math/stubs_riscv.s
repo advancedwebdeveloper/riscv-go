fa01f888eed3e5cda9c6d66fb9bd9bb50828d36f
Joel Sing <joel@sing.id.au>
2019-05-08 02:44:14 +1000

    Add missing math stubs for riscv

--- src/math/stubs_riscv.s
+++ src/math/stubs_riscv.s
@@ -90,8 +90,14 @@ TEXT ·Remainder(SB),NOSPLIT,$0
 TEXT ·Sin(SB),NOSPLIT,$0
 	JMP ·sin(SB)
 
+TEXT ·Sinh(SB),NOSPLIT,$0
+	JMP ·sinh(SB)
+
 TEXT ·Cos(SB),NOSPLIT,$0
 	JMP ·cos(SB)
+
+TEXT ·Cosh(SB),NOSPLIT,$0
+	JMP ·cosh(SB)
 
 TEXT ·Sqrt(SB),NOSPLIT,$0
 	JMP ·sqrt(SB)
